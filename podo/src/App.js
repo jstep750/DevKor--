6ef25e70ef37c1bef04ff202eb9539195b5593c9
yjbae3974 <49051494+yjbae3974@users.noreply.github.com>
2021-08-21 16:50:41 +0900

    .

--- podo/src/App.js
+++ podo/src/App.js
@@ -1,11 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 import './main.scss'
+import {Nav,Container,NavDropdown,Navbar} from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <i class="fas fa-search"></i>
+    <div classNameNameName="App">
+      <Navbar bg="light" expand="">
+  <div className="container-fluid ms-2 me-2" style={{display: 'block'}}>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" className="me-4" />
+    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#link">Link</Nav.Link>
+        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+        </NavDropdown>
+      </Nav>
+    </Navbar.Collapse>
+  </div>
+</Navbar>
     </div>
   );
 }
