c7091bf284d484b9fb34dd1cc343792acb0c1938
yjbae3974 <49051494+yjbae3974@users.noreply.github.com>
2021-08-22 05:09:49 +0900

    .

--- podo/src/components/Play.js
+++ podo/src/components/Play.js
@@ -11,9 +11,13 @@ function Play(){
             <div className="row">
                 <div className="col-lg-7">
                     <div className="youtube-video"></div>
+                    <div className="form-floating mt-3">
+                  <textarea className="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style={{height: 400}}></textarea>
+                  <label for="floatingTextarea2">Comments</label>
+                </div>
                 </div>
                 <div className="col-lg-5" style={{textAlign:'left'}}>
-                    <p>My 재생목록</p>
+                    <p className="pb-2">My 재생목록</p>
                     <div className="playlist-draggable">
                         {
                             재생목록.map((a,i)=>{
@@ -30,15 +34,34 @@ function Play(){
                                 )
                             })
                         }
-                        
+                        <div onClick={()=>{
+                            let 이름 = prompt('새로 생성할 재생목록 이름을 생성해주세요');
+                            if(이름 !== null){
+                                let 아이디 = 재생목록.length + 1;
+                            console.log(아이디);
+                            let copy = [...재생목록];
+                            copy.push({id: 아이디, name: 이름, videoid:[]});
+                            재생목록변경([...copy])
+                            }
+                            
+                        }}><i class="fas fa-plus me-3 pt-1 pb-1"></i>새 재생목록 만들기</div>
+                    </div>
+                    <div className="row mt-3">
+                        <div className="col-6">
+                            <p className="mb-3">현재 동영상의 재생목록</p>
+                            <div className="playlist-draggable">
+                                sdf
+                            </div>
+                        </div>
+                        <div className="col-6">
+                            <p className="mb-3">MY 재생목록 자세히 보기</p>
+                            <div className="playlist-draggable">
+                                sdfsdf
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className="col-lg-7 mt-3">
-                <div className="form-floating">
-  <textarea className="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style={{height: 400}}></textarea>
-  <label for="floatingTextarea2">Comments</label>
-</div>
-                </div>
+                
             </div>
             
             
