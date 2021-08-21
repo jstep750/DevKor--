e02d8aac4aae37b3fd12d0ece1d3934b2f4223ad
yjbae3974 <49051494+yjbae3974@users.noreply.github.com>
2021-08-22 05:41:47 +0900

    searchResult 구현

--- podo/src/components/SearchResult.js
+++ podo/src/components/SearchResult.js
@@ -8,17 +8,45 @@ function SearchResult() {
     {pic: 'https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350', 
     name: 'card one', subsc:'123', movie: '123',ins:'blablablablablablablablablablablablablablablabla'},
   ])
+  let [searchResult, setSearchResult]=useState([
+    {video:[
+      {title:'1',youtuber:'name1', id: 0},
+      {title:'2',youtuber:'name2', id: 1},
+      {title:'3',youtuber:'name3', id: 2},
+      {title:'4',youtuber:'name4', id: 3}],
+    playlist:[
+      '123','456'],
+    userplaylist:[
+      '123','1234']}
+  ])
   let [임시id, set임시id]=useState([
     {id:123456}
   ])
   let 아이디 = 임시id[0].id
+  let  videoList = [...searchResult[0].video];
+  console.log(videoList);
   return (
     <div className="App">
       <div>
-        <div className="row pt-3">
+        {
+          videoList.map((a,i)=>{
+            return(
+              <Card data={data} video={a} i={i}></Card>
+            )
+          })
+        }
+        </div>
+    </div>
+  );
+}
+
+function Card(props){
+  let history = useHistory();
+  return (
+    <div className="row pt-3">
             <div className="col-4" style={{textAlign: 'center'}}>
-                <img className="youtuber-search" src={data[0].pic}></img>
-                <div>유튜버 이름</div>
+                <img className="youtuber-search" src={props.data[0].pic}></img>
+                <div>{props.video.youtuber}</div>
                 <div>구독자 수, 동영상 개수</div>
                 <div>유튜버 설명</div>
             </div>
@@ -30,7 +58,7 @@ function SearchResult() {
                 <div className="d-flex">
                     <div style={{height: 200, width : 300, backgroundColor: 'gray'}}>여기 네모</div>
                     <div>
-                        <div onClick={()=>{history.push('/video/'+아이디)}}>제목</div>
+                        <div onClick={()=>{history.push('/video/'+props.video.id)}}>{props.video.title}</div>
                         <div>조회수, 게시날짜</div>
                         <div>유튜버 아이콘, 유튜버 이름</div>
                         <div>영상 설명</div>
@@ -39,9 +67,7 @@ function SearchResult() {
                 </div>
             </div>
         </div>
-        </div>
-    </div>
-  );
+  )
 }
 
 export default SearchResult;
