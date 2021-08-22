bed0dabcdbc909d3c9ffb92f99894d89ff3c4dde
yjbae3974 <49051494+yjbae3974@users.noreply.github.com>
2021-08-22 11:04:24 +0900

    .

--- podo/src/components/SearchResult.js
+++ podo/src/components/SearchResult.js
@@ -202,16 +202,17 @@ function SearchResult() {
       }]
    }*/
   ])
- 
+  console.log(searchResult)
   let  videoList = [...searchResult[0].video];
   console.log(videoList);
+  console.log(videoList.length)
   return (
     <div className="App">
       <div>
         {
           videoList.map((a,i)=>{
             return(
-              <Card data={data} video={a} i={i}></Card>
+              <Card data={data} video={a} i={i} length={videoList.length}></Card>
             )
           })
         }
@@ -223,27 +224,31 @@ function SearchResult() {
 function Card(props){
   let history = useHistory();
   let 비디오 = props.video;
-  console.log(props.i);
+  let length = 비디오.items.length;
+  let [pos,setpos]=useState(0);
+  console.log(비디오.items)
+  console.log(length);
+  console.log(비디오[0]);
   return (
     <div className="row pt-3">
       <hr />
             <div className="col-4" style={{textAlign: 'center',marginTop:42 ,marginBottom:42}}>
                 <img className="youtuber-search" src={props.data[0].pic}></img>
-                <div className="mb-3 mt-3" style={{color: '#030303',fontSize:18}}>{비디오.channel}</div>
+                <div className="mb-3 mt-3" style={{color: '#030303',fontSize:18}}>{비디오.items[pos].channel}</div>
                 <div className="mb-2" style={{color: '#606060',fontSize:12}}>구독자 수, 동영상 개수</div>
                 <div style={{color: '#606060', fontSize:12}}>유튜버 설명</div>
             </div>
             <div className="col-8" style={{marginTop:42}}>
                 <div className="d-flex"style={{marginBottom:24}}>
-                    <div>키워드가 포함되어 있는 이 유튜버의 영상</div>
-                    <div className="ms-5"><i class="fas fa-caret-left me-3"></i> 1 / {비디오.count} <i class="fas fa-caret-right ms-3"></i></div>
+                    <div>키워드가 포함되어있는 유튜버의 영상</div>
+                    <div className="ms-5"><div></div><i onClick={()=>{if(pos>=1){setpos(pos-1)}}} class="fas fa-caret-left me-3"></i> {pos+1} / {length} <i onClick={()=>{if(pos < length){setpos(pos+1)}}} class="fas fa-caret-right ms-3"></i></div>
                 </div>
                 <div className="d-flex">
-                    <div style={{height: 200, width : 300, backgroundColor: 'gray', marginRight: 16}}>여기 네모</div>
+                    <div style={{height: 200, width : 300, backgroundColor: 'gray', marginRight: 16}}>{비디오.items[pos].title}</div>
                     <div style={{textAlign: 'left'}}>
-                        <div className="mb-2" style={{fontSize:18,color: '#030303'}} onClick={()=>{history.push('/video/'+비디오.items[props.i].title)}}>{비디오.items[props.i].title}</div>
-                        <div className="youtube-subdes mb-3" style={{fontWeight:700}}></div>
-                        <div className="youtube-subdes mb-3 d-flex justify-content-start align-items-center"><div className="circle-1 me-3"></div> {비디오.channel}</div>
+                        <div className="mb-2" style={{fontSize:18,color: '#030303'}} onClick={()=>{history.push('/video/'+비디오.items[pos].link)}}>{비디오.items[pos].title}</div>
+                        
+                        
                         <div className="youtube-subdes mb-2">영상 설명</div>
                         <div className="youtube-subdes">만약 이 영상이 플레이리스트에 있으면 여기 뜸</div>
                     </div>
